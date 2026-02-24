4e95e165a0c37323cbc8833d53f4501c5db26f91
Ryan Matheus <127261627+RyanSantos2002@users.noreply.github.com>
2026-02-24 16:34:25 -0300

    Adicionando Dockerfile para deploy no Render

--- obj/Debug/net9.0/TelerikReportingApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/TelerikReportingApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TelerikReportingApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a98b088a250822df2a6f4ce18241357586a751d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TelerikReportingApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TelerikReportingApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
